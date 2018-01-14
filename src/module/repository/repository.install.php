35585f145406b5003b4c9ad985d1c9de8363de06
Max Svistunov <max.svistunov@gmail.com>
2018-01-14 19:51:16 +0100

    Install script + preview

---
+++ src/module/repository/repository.install.php
@@ -0,0 +1,9 @@
+<?php
+
+function RepositoryInstall() {
+	
+	// Set repository module settings
+    msv_set_config("theme_css_path", "/content/css/repository.css", true);
+    msv_set_config("theme_logo", "/content/images/sitograph-repository-logo-dark-en.png", true);
+	
+}
